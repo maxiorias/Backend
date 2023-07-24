ac973f15cde0991f6344c992f712fe6ea5cb03d4
Maximiliano Orias <maxiorias@gmail.com>
2023-07-24 20:28:47 -0300

    addProduct

--- testing.js
+++ testing.js
@@ -1,5 +1,24 @@
 class ProductManager {
-    constructor() {
-      this.products = [];
+  constructor() {
+    this.products = [];
+  }
+
+  addProduct(product) {
+    if (!this.isValidProduct(product)) {
+      console.error('Datos de producto inválidos.');
+      return;
     }
-  
+
+    if (this.products.some((p) => p.code === product.code)) {
+      console.error(`El producto con código ${product.code} ya existe.`);
+      return;
+    }
+
+    const newProduct = {
+      ...product,
+      id: this.products.length + 1,
+    };
+
+    this.products.push(newProduct);
+    console.log(`Producto con id ${newProduct.id} ha sido agregado exitosamente.`);
+  }
